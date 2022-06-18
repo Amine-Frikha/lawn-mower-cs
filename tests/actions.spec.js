d09d6643c2ba0df48b21347a640a7131e52cb09f
Amine-Frikha <aminfrikha@outlook.com>
2022-06-18 17:40:06 +0200

    unit tests

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -1,55 +1,59 @@
 const { Lawn, Mower } = require("../src/model");
-const { moveLeft, moveRight, moveForward } = require("../src/actions");
 
 describe("actions", () => {
-  let currentMower, lawn;
+  let mower, lawn;
 
   beforeEach(() => {
-    currentMower = new Mower(1, 1, "N");
+    mower = new Mower(1, 1, "N");
     lawn = new Lawn(5, 5);
   });
 
   it("should move to the left", () => {
-    currentMower = moveLeft(currentMower);
-    expect(currentMower).toEqual(new Mower(1, 1, "W"));
+    mower.moveLeft();
+    expect(mower).toEqual(new Mower(1, 1, "W"));
     ["S", "E", "N"].forEach((expected) => {
-      currentMower = moveLeft(currentMower);
-      expect(currentMower.dir).toBe(expected);
-      return currentMower;
+      mower.moveLeft();
+      expect(mower.dir).toBe(expected);
+      return mower;
     });
   });
 
   it("should move to the right", () => {
-    currentMower = moveRight(currentMower);
-    expect(currentMower).toEqual(new Mower(1, 1, "E"));
+    mower.moveRight();
+    expect(mower).toEqual(new Mower(1, 1, "E"));
     ["S", "W", "N"].forEach((expected) => {
-      currentMower = moveRight(currentMower);
-      expect(currentMower.dir).toBe(expected);
-      return currentMower;
+      mower.moveRight();
+      expect(mower.dir).toBe(expected);
+      return mower;
     });
   });
 
   it("should not move when outside the lawn", () => {
-    let currentMower = new Mower(0, 0, "S");
-    expect(moveForward(currentMower, lawn)).toBe(currentMower);
-    currentMower = new Mower(0, 0, "W");
-    expect(moveForward(currentMower, lawn)).toBe(currentMower);
-    currentMower = new Mower(5, 5, "N");
-    expect(moveForward(currentMower, lawn)).toBe(currentMower);
-    currentMower = new Mower(5, 0, "E");
-    expect(moveForward(currentMower, lawn)).toBe(currentMower);
+    let mower = new Mower(0, 0, "S");
+    mower.moveForward();
+    expect(mower).toBe(mower);
+    mower = new Mower(0, 0, "W");
+    mower.moveForward();
+    expect(mower).toBe(mower);
+    mower = new Mower(5, 5, "N");
+    mower.moveForward();
+    expect(mower).toBe(mower);
+    mower = new Mower(5, 0, "E");
+    mower.moveForward();
+    expect(mower).toBe(mower);
   });
 
   it("should move forward", () => {
-    expect(moveForward(currentMower, lawn)).toEqual(new Mower(1, 2, "N"));
-    expect(moveForward(new Mower(3, 3, "E"), lawn)).toEqual(
-      new Mower(4, 3, "E")
-    );
-    expect(moveForward(new Mower(3, 3, "W"), lawn)).toEqual(
-      new Mower(2, 3, "W")
-    );
-    expect(moveForward(new Mower(3, 3, "S"), lawn)).toEqual(
-      new Mower(3, 2, "S")
-    );
+    mower.moveForward();
+    expect(mower).toEqual(new Mower(1, 2, "N"));
+    let mower2 = new Mower(3, 3, "E");
+    mower2.moveForward();
+    expect(mower2).toEqual(new Mower(4, 3, "E"));
+    let mower3 = new Mower(3, 3, "W");
+    mower3.moveForward();
+    expect(mower3).toEqual(new Mower(2, 3, "W"));
+    let mower4 = new Mower(3, 3, "S");
+    mower4.moveForward();
+    expect(mower4).toEqual(new Mower(3, 2, "S"));
   });
 });
