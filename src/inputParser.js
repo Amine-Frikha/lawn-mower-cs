76e4bf74768881111f7d7c37c60cb000d5806c0b
Amine-Frikha <aminfrikha@outlook.com>
2022-06-19 21:12:48 +0200

    bugs

--- src/inputParser.js
+++ src/inputParser.js
@@ -1,4 +1,4 @@
-/** Checks lawn's dimensions are positive and not both zero
+/** Checks lawn's dimensions are > 0 before parsing input
  * @param  {string} line
  * @return {Array}
  */
@@ -6,14 +6,14 @@ const parseLawnInput = line => {
   const dimensions = line.split(' ');
   x = parseInt(dimensions[0]);
   y = parseInt(dimensions[1]);
-  if (x > 0 && y > 0) {
+  if (x > 0 && y > 0 && dimensions.length === 2) {
     return dimensions;
   } else {
     throw new Error('Invalid lawn dimensions : ' + line);
   }
 };
 
-/** Checks mower initial coordinates are inside the grid
+/** Checks mower initial coordinates are inside the grid before parsing input
  * @param  {string} line
  * @return {Array}
  */
@@ -21,13 +21,14 @@ const parseMowerInput = (lawn, line) => {
   const coordinates = line.split(' ');
   x = parseInt(coordinates[0]);
   y = parseInt(coordinates[1]);
-  if (x <= lawn.x && y <= lawn.y) {
+  if (x <= lawn.x && y <= lawn.y && coordinates.length === 3) {
     return coordinates;
   } else {
     throw new Error('initial position outside of lawn : ' + line);
   }
 };
-/** Checks mower initial coordinates are inside the grid
+
+/** Parses actions
  * @param  {string} line
  * @return {Array}
  */
